470b87265eb045f741e4991180bd09efc8a85888
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2024-10-16 20:59:54 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,29 +4,11 @@ import postcss from 'rollup-plugin-postcss';
 
 export default defineConfig({
   base: './',
-  plugins: [react(), postcss()],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     minify: true,
     sourcemap: true,
     assetsDir: 'assets',
   },
-  server: {
-    port: 3000,
-    open: true,
-    strictPort: true,
-    fs: {
-      cache: 'mem',
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        require('postcss-webkit-text-size-adjust')({
-          standardProperty: true,
-        }),
-        require('autoprefixer'), // Optional, for broader compatibility
-      ],
-    },
-  },
-});
+})
