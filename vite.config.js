2d6d135355286a34cb1cfb237af05351fb72fdc7
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2024-10-16 14:35:07 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // or remove this line for relative paths
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    minify: true,
+    sourcemap: true,
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
+    open: true,
+    strictPort: true,
+    fs: {
+      cache: 'mem',
+    },
+  },
+});
